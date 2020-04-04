c248ddca22cdaca6eaf567fa616866ba75df1798
Davinci Lisapaly <davincilisapaly@visionplanner.com>
2020-04-04 21:01:46 +0200

    Added new classes for Person, Librarian & Subscriber. Further developer SubscribersRepo. Okee doei homos

--- Objects/Person.py
+++
@@ -1,5 +0,0 @@
-# Person.py is een class wat overgeerft wordt door andere classes, nou dat betekent dus dat er nog 2 classes toegevoegd moeten
-# Mag je lekker naar de class diagram kijken om welke 2 classes het gaat
-# Succes, Okee doei 
-
-
