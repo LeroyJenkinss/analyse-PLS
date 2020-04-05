707e5ea3047f7f178ad5274dbe9de9de94d1457b
Davinci Lisapaly <davincilisapaly@visionplanner.com>
2020-04-05 17:53:44 +0200

    Added ViewCatalog method to Catalog class, kapot lelijke code maar dikke boeiaa

--- Views/Catalog.py
+++ Views/Catalog.py
@@ -12,7 +12,39 @@ class Catalog:
 
     def ViewCatalog(self):
         self.__init__()
+        bookRowView = ""
+        whiteSpaceToAddTitle, whiteSpaceToAddAuthor, whiteSpaceToAddGenre = 0, 0, 0
+
         for book in self.AllBooksList:
-            print(book.Title)
+            if len(book.Title) > whiteSpaceToAddTitle:
+                whiteSpaceToAddTitle = len(book.Title)
+            if len(book.Author) > whiteSpaceToAddAuthor:
+                whiteSpaceToAddAuthor = len(book.Author)
+            if len(book.Genre) > whiteSpaceToAddGenre:
+                whiteSpaceToAddGenre = len(book.Genre)
+
+        for book in self.AllBooksList:
+            if int(book.Id) > 9:
+                whiteSpaceAfterId = " "*6
+            else:
+                whiteSpaceAfterId = " "*7
+                
+            whiteSpaceAfterTitle = (" " * (whiteSpaceToAddTitle-len(book.Title)))
+            whiteSpaceAfterAuthor = (" " * (whiteSpaceToAddAuthor-len(book.Author)))
+            whiteSpaceAfterGenre = (" " * (whiteSpaceToAddAuthor-len(book.Genre)))
+
+            bookRowView += "|| {}{}|| {}{}|| {}{}|| {}{}||\n".format(book.Id, whiteSpaceAfterId, 
+                                                                     book.Title, whiteSpaceAfterTitle, 
+                                                                     book.Author, whiteSpaceAfterAuthor, 
+                                                                     book.Genre, whiteSpaceAfterGenre)
+        
+        columnTitleFormatting = "|| BookId: || Title:{}|| Author:{}|| Genre:{}||".format(" "*(whiteSpaceToAddTitle-6),
+                                                                                         " "*(whiteSpaceToAddAuthor-7),
+                                                                                         " "*(whiteSpaceToAddGenre-4))
+
+        print("="*len(columnTitleFormatting))
+        print(columnTitleFormatting)
+        print(bookRowView[:-1])
+        print("="*len(columnTitleFormatting))
 
 Catalog().ViewCatalog()
